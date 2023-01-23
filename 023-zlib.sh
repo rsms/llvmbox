76c4461c1f0b8f6ec8fb9bae3ee3b5c92cbe2fc7
Rasmus Andersson <rasmus@notion.se>
2023-01-23 12:48:12 -0800

    fix 023-zlib.sh

--- 023-zlib.sh
+++ 023-zlib.sh
@@ -2,18 +2,6 @@
 set -euo pipefail
 source "$(dirname "$0")/config.sh"
 
-cat << _END_
-TARGET                   $TARGET
-TARGET_ARCH              $TARGET_ARCH
-TARGET_SYS               $TARGET_SYS
-TARGET_SYS_VERSION       $TARGET_SYS_VERSION
-TARGET_SYS_MINVERSION    $TARGET_SYS_MINVERSION
-TARGET_SYS               $TARGET_SYS
-TARGET_TRIPLE            $TARGET_TRIPLE
-TARGET_CMAKE_SYSTEM_NAME $TARGET_CMAKE_SYSTEM_NAME
-_END_
-exit
-
 _fetch_source_tar \
   https://zlib.net/zlib-${ZLIB_VERSION}.tar.gz "$ZLIB_SHA256" "$ZLIB_SRC"
 
